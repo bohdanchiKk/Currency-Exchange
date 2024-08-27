751ecfdcddf7f52a772469a361e5c220161829d5
bohdan <bogdanrizko@gmail.com>
2024-08-27 12:54:38 +0300

    commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const host = "http://localhost:8080/currency_exchange_war_exploded"
+    const host = "http://localhost:8080/api"
 
     // Fetch the list of currencies and populate the select element
     function requestCurrencies() {
